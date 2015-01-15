9fe792e9e458b1c6a68aafa3d81201874fa7f911
Mykamu <fodjus@yahoo.fr>
2015-01-15 23:21:18 +0100

    Initial

---
+++ university/src/fr/sewatech/university/service/CourseService.java
@@ -0,0 +1,11 @@
+package fr.sewatech.university.service;
+
+import java.util.List;
+
+import fr.sewatech.university.model.Course;
+
+public interface CourseService {
+	List<Course> findAll();
+	Course findById(Integer id);
+	void save(Course course);
+}
---
+++ university/src/fr/sewatech/university/service/CourseServiceImpl.java
@@ -0,0 +1,36 @@
+package fr.sewatech.university.service;
+
+import java.util.List;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import fr.sewatech.university.model.Course;
+
+@Service("courseService")
+@Transactional
+public class CourseServiceImpl implements CourseService {
+
+	@Autowired
+	private SessionFactory sessionFactory;
+		
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Course> findAll() {
+		return sessionFactory.getCurrentSession().createQuery("from Course").list();
+	}
+
+	@Override
+	public Course findById(Integer id){
+		return (Course) sessionFactory.getCurrentSession().get(Course.class, id);
+	}
+
+	@Override
+	public void save(Course course) {
+		sessionFactory.getCurrentSession().saveOrUpdate(course);
+
+	}
+
+}
